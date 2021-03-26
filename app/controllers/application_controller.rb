7153945cca1a5ea5d5eb93d66bb6e3cd1a84e9fa
Tinkergit <nicholas.stagno@gmail.com>
2021-03-26 01:04:49 -0400

    stage 1 on fixing html elements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,10 +30,6 @@ class ApplicationController < Sinatra::Base
       def logged_in?
         !!current_user          ####!!session[:user_id]
       end
-  
-      # def current_user
-      #   User.find(session[:user_id])
-      # end
     end
   
 end
--- app/controllers/ghibli_controller.rb
+++ app/controllers/ghibli_controller.rb
@@ -1,7 +1,6 @@
 class GhibliController < ApplicationController
 
     get '/ghiblis' do
-        redirect_if_not_logged_in
 
         @ghiblis = Ghibli.all
         erb :'ghiblis/index'
