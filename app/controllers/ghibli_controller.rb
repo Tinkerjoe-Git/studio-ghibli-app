44c6ba72cf40ff7c7605e15e5ec4f518991d6cd9
Tinkergit <nicholas.stagno@gmail.com>
2021-03-27 00:58:01 -0400

    Trying to make deets a metaprogram with title link

--- app/controllers/ghibli_controller.rb
+++ app/controllers/ghibli_controller.rb
@@ -5,4 +5,12 @@ class GhibliController < ApplicationController
         @ghiblis = Ghibli.all
         erb :'ghiblis/index'
     end
+
+    ##Lets try and have the dropdown show all the film titles in english, and make those all hrefs
+    ##Then the user clicks the href and it takes them to the films details
+
+    get '/ghiblis/deets'do
+        @ghibli = Ghibli.find_by(:title => params[:title])
+        erb :'ghiblis/deets'
+    end
 end
