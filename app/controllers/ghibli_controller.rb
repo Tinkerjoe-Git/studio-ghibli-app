5754b72181a386337ca59005bac9c6ad6e48b97d
Tinkergit <nicholas.stagno@gmail.com>
2021-03-28 20:15:24 -0400

    Fixed up the Navbar, Can fetch film details

--- app/controllers/ghibli_controller.rb
+++ app/controllers/ghibli_controller.rb
@@ -3,15 +3,20 @@ class GhibliController < ApplicationController
     get '/ghiblis' do
 
         @ghiblis = Ghibli.all
-        erb :'ghiblis/index'
+        erb :'ghiblis/deets'
     end
 
     ##Lets try and have the dropdown show all the film titles in english, and make those all hrefs
     ##Then the user clicks the href and it takes them to the films details
 
-    get '/ghiblis/deets'do
-        @ghibli = Ghibli.find_by(:title => params[:params])
-        erb :'ghiblis/deets'
+    # get '/ghiblis/deets'do
+    #     @ghiblis = Ghibli.all
+    #     erb :'ghiblis/deets'
+    # end
+
+    get '/ghiblis/:id' do
+        @ghibli = Ghibli.find_by_id(params[:id])
+        erb :"/ghiblis/show"
     end
 
     post '/ghiblis/search' do
